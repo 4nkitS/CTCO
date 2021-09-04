e17430898faf60624acaffe9aeff313be1aac5c5
Ankit Shukla <shukla.ankit@protonmail.com>
2021-09-04 07:16:55 -0400

    P3

--- Python/src/arrays_and_strings/p3.py
+++ Python/src/arrays_and_strings/p3.py
@@ -2,26 +2,59 @@
 from typing import Dict
 
 
-def URLify(word: str, length: int) -> str:
-    count_spaces = 0
+# Not in place substitution?
+def URLify1(word: str, length: int) -> str:
+    list_words = word.strip().split(' ')
+    ret = ''
+    for word in list_words:
+        if ret != '':
+            ret += '%20'
+        ret += word
+    return ret
+
+
+# SLOW
+def URLify2(word: str, length: int) -> str:
+    count_preceding_spaces = 0
     for i in range(length):
         if word[i] == ' ':
-            count_spaces += 1
-    assert(length + 2 * count_spaces == len(word))
+            count_preceding_spaces += 1
+    assert(length + 2 * count_preceding_spaces == len(word))
+
+    word = list(word)
+    for i in range(length):
+        if count_preceding_spaces == 0:
+            break
+        if word[length - 1 - i] != ' ':
+            word[length - 1 + 2 * count_preceding_spaces - i] = word[length - 1 - i]
+        else:
+            word[length - 1 + 2 * count_preceding_spaces - i - 2] = '%'
+            word[length - 1 + 2 * count_preceding_spaces - i - 1] = '2'
+            word[length - 1 + 2 * count_preceding_spaces - i] = '0'
+            count_preceding_spaces -= 1
+    return ''.join(word)
+
+
+def URLify3(word: str, length: int) -> str:
+    count_preceding_spaces = 0
+    for i in range(length):
+        if word[i] == ' ':
+            count_preceding_spaces += 1
+    assert(length + 2 * count_preceding_spaces == len(word))
     for i in range(length):
         if word[length - 1 - i] != ' ':
-            word[length + 2 * count_spaces - 1 - i] = word[length - 1 - i]
+            word = word[:length - 1 + 2 * count_preceding_spaces - i] + word[length - 1 - i] + word[length - 1 + 2 * count_preceding_spaces - i + 1:]
         else:
-            count_spaces -= 1
-            word[length + 2 * count_spaces - 1 - i] = '%'
-            word[length + 2 * count_spaces - 1 - i + 1] = '2'
-            word[length + 2 * count_spaces - 1 - i + 2] = '0'
+            count_preceding_spaces -= 1
+            word = word[:length - 1 + 2 * count_preceding_spaces - i] + '%20' + word[length - 1 + 2 * count_preceding_spaces - i + 3:]
+            if count_preceding_spaces == 0:
+                break
     return word
 
 
 def P3():
     for test in list_inputs:
-        print('"' + test["string"] + '" of length ' + str(test["length"]) + ' URLify to ' + URLify(test["string"], test["length"]) + '.')
+        print('"' + test["string"] + '" of length ' + str(test["length"]) + ' URLify to ' + URLify3(test["string"], test["length"]))
 
 
 list_inputs = [{"string": 'Mr John Smith    ', "length": 13}, {"string": 'a b  ', "length": 3}]
